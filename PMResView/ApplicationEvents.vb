7ee1587570f442648b3487e0389137ca6ed63256
Andy <webmaster@RealityRipple.com>
2024-01-31 11:08:15 -0800

    Improve Authenticode Error Display

--- PMResView/ApplicationEvents.vb
+++ PMResView/ApplicationEvents.vb
@@ -10,7 +10,9 @@
     Private Sub MyApplication_Startup(sender As Object, e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
       Dim v As Authenticode.Validity = Authenticode.IsSelfSigned(Reflection.Assembly.GetExecutingAssembly().Location)
       If Not (v = Authenticode.Validity.SignedAndValid Or v = Authenticode.Validity.SignedButUntrusted) Then
-        If SuperMsgBox(Nothing, "Authenticode Failure", Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardIcon.Error, "Application integrity not guaranteed.", "The Executable """ & IO.Path.GetFileName(Reflection.Assembly.GetExecutingAssembly().Location) & """ is not signed and may be corrupted or modified." & vbNewLine & "Would you like to continue loading " & My.Application.Info.ProductName & " anyway?", Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardButtons.Yes Or Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardButtons.No, "Error Code: 0x" & Hex(v) & " " & v.ToString, Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardIcon.Warning) = Microsoft.WindowsAPICodePack.Dialogs.TaskDialogResult.No Then e.Cancel = True
+        Dim sErr As String = "0x" & v.ToString("x")
+        If Not CStr(v) = v.ToString Then sErr = v.ToString & " (0x" & v.ToString("x") & ")"
+        If SuperMsgBox(Nothing, "Authenticode Failure", Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardIcon.Error, "Application integrity not guaranteed.", "The Executable """ & IO.Path.GetFileName(Reflection.Assembly.GetExecutingAssembly().Location) & """ is not signed and may be corrupted or modified." & vbNewLine & "Would you like to continue loading " & My.Application.Info.ProductName & " anyway?", Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardButtons.Yes Or Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardButtons.No, "Error Code: " & sErr, Microsoft.WindowsAPICodePack.Dialogs.TaskDialogStandardIcon.Warning) = Microsoft.WindowsAPICodePack.Dialogs.TaskDialogResult.No Then e.Cancel = True
       End If
     End Sub
   End Class
